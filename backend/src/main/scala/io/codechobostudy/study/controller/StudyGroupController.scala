f06744987f1e0ffacf9a74074cfb048e158e2caf
Changhwa Oh <changhwaoh.co@gmail.com>
2015-05-13 21:15:49 +0900

    feature(study) : 컨트롤러에서 리턴 타입 변경 1. Jackson 의 ObjectMapper를 이용해 리턴 -> 도메인 자체 리턴

--- backend/src/main/scala/io/codechobostudy/study/controller/StudyGroupController.scala
+++ backend/src/main/scala/io/codechobostudy/study/controller/StudyGroupController.scala
@@ -1,6 +1,5 @@
 package io.codechobostudy.study.controller
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import io.codechobostudy.study.domain.StudyGroup
 import io.codechobostudy.study.service.StudyGroupService
 import org.springframework.beans.factory.annotation.Autowired
@@ -11,26 +10,23 @@ import org.springframework.web.bind.annotation._
 class StudyGroupController {
 
   @Autowired
-  private var objectMapper: ObjectMapper = _
-
-  @Autowired
   private var studyGroupService: StudyGroupService = _
 
   @RequestMapping(value = Array("/"),  method = Array(RequestMethod.POST))
-  def groupCreate(@RequestBody studyGroup: StudyGroup): String = {
-    objectMapper.writeValueAsString(studyGroupService.createGroup(studyGroup))
+  def groupCreate(@RequestBody studyGroup: StudyGroup): StudyGroup = {
+    studyGroupService.createGroup(studyGroup)
   }
 
   @RequestMapping(value = Array("/{id}"), method= Array(RequestMethod.GET))
-  def groupShow(@PathVariable id: java.lang.Long): String = {
-    objectMapper.writeValueAsString(studyGroupService.showGroup(id))
+  def groupShow(@PathVariable id: java.lang.Long): StudyGroup= {
+    studyGroupService.showGroup(id)
   }
 
   @RequestMapping(value = Array("/{id}"), method = Array(RequestMethod.PUT))
   def groupUpdate(@PathVariable id: java.lang.Long,
-             @RequestBody studyGroup: StudyGroup): String = {
+             @RequestBody studyGroup: StudyGroup): StudyGroup = {
     studyGroup.id = id
-    objectMapper.writeValueAsString(studyGroupService.updateGroup(studyGroup))
+    studyGroupService.updateGroup(studyGroup)
   }
 
   @RequestMapping(value = Array("/{id}"), method = Array(RequestMethod.DELETE))
