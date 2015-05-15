ab814033dac062e96e87a57d41b3f01b3af2851d
jinhyun <free1330@gmail.com>
2015-05-15 18:14:42 +0900

    refactor(Notification): TempCode를 Mock Object로 구현

--- backend/src/test/java/io/codechobostudy/notifications/NotiTest.java
+++ backend/src/test/java/io/codechobostudy/notifications/NotiTest.java
@@ -3,6 +3,7 @@ package io.codechobostudy.notifications;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.codechobostudy.Application;
 import io.codechobostudy.notifications.domain.NotiView;
+import io.codechobostudy.notifications.repository.MockNotiViewRepository;
 import io.codechobostudy.notifications.service.NotiService;
 import org.junit.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,8 +29,8 @@ public class NotiTest {
         ObjectMapper objectMapper = new ObjectMapper();
         NotiView jsonObject = objectMapper.readValue(jsonString, NotiView.class);
 
-        assertThat(notiService.firstContents, is(jsonObject.getNotiList().get(0).getContents()));
-        assertThat(notiService.secondContents, is(jsonObject.getNotiList().get(1).getContents()));
-        assertThat(notiService.totalCnt, is(jsonObject.getNotiCnt().getTotalCnt()));
+        assertThat(MockNotiViewRepository.firstContents, is(jsonObject.getNotiList().get(0).getContents()));
+        assertThat(MockNotiViewRepository.secondContents, is(jsonObject.getNotiList().get(1).getContents()));
+        assertThat(MockNotiViewRepository.totalCnt, is(jsonObject.getNotiCnt().getTotalCnt()));
     }
 }
