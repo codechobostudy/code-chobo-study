66a513db844db3161608c43e6d49644ff95a9733
jinhyun <free1330@gmail.com>
2015-05-15 17:49:54 +0900

    bugfix(Notification): Autowired 설정 오류수정

--- backend/src/test/java/io/codechobostudy/notifications/NotiTest.java
+++ backend/src/test/java/io/codechobostudy/notifications/NotiTest.java
@@ -1,23 +1,28 @@
 package io.codechobostudy.notifications;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.codechobostudy.Application;
 import io.codechobostudy.notifications.domain.NotiView;
 import io.codechobostudy.notifications.service.NotiService;
 import org.junit.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.io.IOException;
+import org.junit.runner.RunWith;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
 public class NotiTest {
     @Autowired
     private NotiService notiService;
 
     @Test
     public void testGetNotiViewJson() throws IOException {
-        notiService = new NotiService();    // Autowired 설정했는데도 오류
         String jsonString = notiService.getNotiViewJsonString();
 
         ObjectMapper objectMapper = new ObjectMapper();
