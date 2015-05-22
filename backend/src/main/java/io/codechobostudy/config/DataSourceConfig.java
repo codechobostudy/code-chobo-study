a94663409ad7a0c0eed331871d684655eedaad57
youngit <kimgane02@gmail.com>
2015-05-23 06:38:13 +0900

    test(user): Embedded DataBase Web Console 추가

--- backend/src/main/java/io/codechobostudy/config/DataSourceConfig.java
+++ backend/src/main/java/io/codechobostudy/config/DataSourceConfig.java
@@ -1,7 +1,10 @@
 package io.codechobostudy.config;
 
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
@@ -15,8 +18,17 @@ public class DataSourceConfig {
     public DataSource userDataSource() {
         return new EmbeddedDatabaseBuilder().
             setType(EmbeddedDatabaseType.H2).
+            setName("test").
             addScript("classpath:schema/user.sql").
             build();
+    }
+
+    @Bean
+    @Profile("dev")
+    public ServletRegistrationBean h2servletRegistration() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
+        registration.addUrlMappings("/console/*");
+        return registration;
     }
 
 }
--- backend/src/main/java/io/codechobostudy/config/SecurityConfig.java
+++ backend/src/main/java/io/codechobostudy/config/SecurityConfig.java
@@ -1,16 +1,12 @@
 package io.codechobostudy.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-import javax.sql.DataSource;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 
 @Configuration
 @EnableWebSecurity
@@ -20,6 +16,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+            .headers()
+                .addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))
+                .cacheControl()
+                .and()
             .formLogin()
                 .loginPage("/signin")
                 .failureUrl("/signin?error=bad_credentials")
@@ -42,7 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) throws Exception {
         web
             .ignoring()
-            .antMatchers("/favicon.ico", "/style/**", "/js/**", "/images/**", "/api/sample/**");
+            .antMatchers("/favicon.ico", "/style/**", "/js/**", "/images/**", "/api/sample/**", "/console/**");
     }
 
 }
