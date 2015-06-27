4ec427f889a9d136212a2a94d5f872787085f6d6
KangYong <arahansa@naver.com>
2015-06-28 01:08:01 +0900

    자바스크립트/spinner 달아보는 연습.

---
+++ frontend/assets/js/app.js
@@ -0,0 +1,69 @@
+
+/*
+Version: 0.01
+Author: arhansa (arahansa@naver.com)
+
+Comment: I studied by Template (
+Template Name: Color Admin - Responsive Admin Dashboard Template build with Twitter Bootstrap 3.3.4
+Version: 1.7.0
+Author: Sean Ngu
+Website: http://www.seantheme.com/color-admin-v1.7/admin/
+)
+    ----------------------------
+        APPS CONTENT TABLE
+    ----------------------------
+    
+    <!-- ======== GLOBAL SCRIPT SETTING ======== -->
+    01. Handle Scrollbar
+    
+    02. Handle Sidebar - Menu
+    03. Handle Sidebar - Mobile View Toggle
+    04. Handle Sidebar - Minify / Expand
+    05. Handle Page Load - Fade in
+    06. Handle Panel - Remove / Reload / Collapse / Expand
+    07. Handle Panel - Draggable
+    08. Handle Tooltip & Popover Activation
+    09. Handle Scroll to Top Button Activation
+    
+    <!-- ======== Added in V1.2 ======== -->
+    10. Handle Theme & Page Structure Configuration
+    11. Handle Theme Panel Expand
+    12. Handle After Page Load Add Class Function - added in V1.2
+    
+    <!-- ======== Added in V1.5 ======== -->
+    13. Handle Save Panel Position Function - added in V1.5
+    14. Handle Draggable Panel Local Storage Function - added in V1.5
+    15. Handle Reset Local Storage - added in V1.5
+    
+    <!-- ======== Added in V1.6 ======== -->
+    16. Handle IE Full Height Page Compatibility - added in V1.6
+    17. Handle Unlimited Nav Tabs - added in V1.6
+    
+    <!-- ======== Added in V1.7 ======== -->
+    18. Handle Mobile Sidebar Scrolling Feature - added in V1.7
+	
+    <!-- ======== APPLICATION SETTING ======== -->
+    Application Controller
+*/
+
+
+/* 05. Handle Page Load - Fade in
+------------------------------------------------ */
+var handlePageContentView = function() {
+    "use strict";
+    $.when($('#page-loader').addClass('hide')).done(function() {
+        $('#page-container').addClass('in');
+    });
+};
+
+/* Application Controller
+------------------------------------------------ */
+var App = function () {
+	"use strict";
+	return {
+		//main function
+		init: function () {
+			handlePageContentView();
+		}
+  };
+}();
